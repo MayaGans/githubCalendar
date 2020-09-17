e4b661d777ce6658cbba35596fee0c2d3ca84b49
Maya Gans <jaffe.maya@gmail.com>
2020-09-17 08:09:19 -0700

    add d3 as a dependency

--- R/hello.R
+++ R/hello.R
@@ -1,18 +1,3 @@
-# Hello, world!
-#
-# This is an example function named 'hello' 
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Cmd + Shift + B'
-#   Check Package:             'Cmd + Shift + E'
-#   Test Package:              'Cmd + Shift + T'
-
-hello <- function() {
-  print("Hello, world!")
-}
+# dir.create("./inst/htmlwidgets/d3", recursive = TRUE)
+# d3 <- paste0("https://d3js.org/d3.v6.js")
+# download.file(d3, "./inst/htmlwidgets/d3/d3.min.js")
